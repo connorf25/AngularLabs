20ac1a99bc725121d39db19e20b9b380005fb656
connorf25 <connorforbes25@gmail.com>
2019-09-30 17:06:41 +1000

    Update addUserToServer.js

--- server/api/addUserToServer.js
+++ server/api/addUserToServer.js
@@ -9,6 +9,7 @@ module.exports = function(db, app, ObjectID) {
 
         var username = req.username
         var servername = req.servername
+        var num = 0
         console.log("Adding: ", username, " to: ", servername)
 
         const collection = db.collection('users');
@@ -27,82 +28,17 @@ module.exports = function(db, app, ObjectID) {
                 }
                 collection.insertOne(newuser, (err,dbres) => {
                     if (err) throw err;
-                    let num = dbres.insertedCount;
+                    num = dbres.insertedCount;
                     res.send({'num': num, err: null})
                 })
-                // TODO Add to server
             } else {
-                // TODO User Exists, add to server
+                // TODO User Exists, push to grouplist
                 res.send({num:0, err:"duplicate item"});
+                collection.updateOne(
+                    {username: username},
+                    {$push: {groupList: servername}}
+                )
             }
         })
     })
-
-    console.log("AddUserToServer request recieved")
-    var input = req.body;
-    var username = input.username
-    var servername = input.servername
-    var users;
-    // Add some kind of authentication
-    console.log("Adding: ", username, " to: ", servername)
-
-    fs.readFile('./server/data/users.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err)
-            res.send("Error reading file from disk");
-            return
-        }
-        try {
-            users = JSON.parse(jsonString)
-
-            for (i in users) {
-                if (username == users[i].username) {
-                    console.log("User already exists")
-                    users[i].groupList.push(servername)
-                    // Write changes to server
-                    jsonString = JSON.stringify(users)
-                    fs.writeFile('./server/data/users.json', jsonString, err => {
-                        if (err) {
-                            console.log('Error writing file', err)
-                            res.send("Error writing updated user");
-                        } else {
-                            console.log('Successfully wrote file')
-                            res.send("Updated GroupList")
-                        }
-                    })
-                    return;
-                }
-            }
-            var newuser = {
-                username: username,
-                email: "",
-                pw: "",
-                supp: false,
-                ofGroupAdminsRole: false,
-                groupList: [ servername ]
-            }
-            console.log("User does not exist, adding user")
-            users.push(newuser)
-            console.log("Added: ", newuser)
-
-            jsonString = JSON.stringify(users)
-            fs.writeFile('./server/data/users.json', jsonString, err => {
-                if (err) {
-                    console.log('Error writing file', err)
-                    res.send("Error writing new user");
-                } else {
-                    console.log('Successfully wrote file')
-                    res.send("New User added to server");
-                }
-            })
-        } catch(err) {
-            res.send('Error parsing JSON string');
-            console.log('Error parsing JSON string:', err)
-        }
-    })
-
-    if (!req.body) {
-        console.log("Error: no request body")
-        return res.sendStatus(400);
-    }
 }
