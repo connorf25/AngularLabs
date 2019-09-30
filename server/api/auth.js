df754b6aec570fe46da3be105a92a8923b3a134f
connorf25 <connorforbes25@gmail.com>
2019-09-30 17:27:05 +1000

    Fix server.js and auth.js

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,28 +1,30 @@
 const fs = require('fs')
 // Usage: this.httpClient.post('http://localhost:3000/api/auth', user, httpOptions)
-module.exports = function(req, res) {
-    console.log("Login request recieved")
-    var customer = {};
-
-    customer.username = req.body.username;
-    customer.pw = req.body.pw;
-    customer.valid = false;
-
-    const collection = db.collection('users');
-
-    collection.findOne({username: customer.username}).exec(function(err, user) {
-        if( user.pw == customer.pw ) {
-            customer = user
-            customer.valid = true
+module.exports = function(db, app) {
+    app.post('api/auth', (req, res) => {
+        if (!req.body) {
+            console.log("Error: no request body")
+            return res.sendStatus(400);
         }
-        console.log(customer)
+    
+        console.log("Login request recieved")
+        var customer = {};
+    
+        customer.username = req.body.username;
+        customer.pw = req.body.pw;
+        customer.valid = false;
+    
+        const collection = db.collection('users');
+    
+        collection.findOne({username: customer.username}).exec(function(err, user) {
+            if( user.pw == customer.pw ) {
+                customer = user
+                customer.valid = true
+            }
+            console.log(customer)
+            res.send(customer)
+        })
+        console.log("User does not exist")
         res.send(customer)
     })
-    console.log("User does not exist")
-    res.send(customer)
-
-    if (!req.body) {
-        console.log("Error: no request body")
-        return res.sendStatus(400);
-    }
 }
--- server/server.js
+++ server/server.js
@@ -24,64 +24,15 @@ MongoClient.connect(url, { poolSize:10, useNewUrlParser: true, useUnifiedTopolog
 
         require('./api/auth.js')(db, app);
 
-        require('./api/getGroup.js')(db, app, ObjectID);
-        require('./api/getUsers.js')(db, app);
+        // require('./api/getGroup.js')(db, app, ObjectID);
+        // require('./api/getUsers.js')(db, app);
 
-        require('./api/removeGroup.js')(db, app, ObjectID);
-        require('./api/removeUserFromServer.js')(db, app, ObjectID);
+        // require('./api/removeGroup.js')(db, app, ObjectID);
+        // require('./api/removeUserFromServer.js')(db, app, ObjectID);
 
-        require('./api/updateGroup.js')(db, app, ObjectID);
-        require('./api/updateUser.js')(db, app, ObjectID);
-        require('./api/updateUsers.js')(db, app, ObjectID);
+        // require('./api/updateGroup.js')(db, app, ObjectID);
+        // require('./api/updateUser.js')(db, app, ObjectID);
+        // require('./api/updateUsers.js')(db, app, ObjectID);
 
     require('./listen.js')(http);
-})
-
-
-
-
-
-
-const express = require('express');
-const app = express();
-
-const cors = require('cors');
-app.use(cors());
-
-// const path = require('path')
-// const http = require('http').Server(app)
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
-// MongoDB
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017";
-const dbName = 'chat';
-const client = new MongoClient(url);
-
-client.connect((err) => {
-    console.log("Client successfully connected");
-    const db = client.db(dbName)
-    client.close();
-})
-
-
-
-app.post('/api/auth', require('./api/auth.js'))
-app.post('/api/addUser', require('./api/addUser.js'))
-app.post('/api/getUsers', require('./api/getUsers.js'))
-app.post('/api/updateUsers', require('./api/updateUsers.js'))
-app.post('/api/getGroup', require('./api/getGroup.js'))
-app.post('/api/updateGroup', require('./api/updateGroup.js'))
-app.post('/api/updateUser', require('./api/updateUser.js'))
-app.post('/api/addUserToServer', require('./api/addUserToServer.js'))
-app.post('/api/removeUserFromServer', require('./api/removeUserFromServer.js'))
-app.post('/api/removeGroup', require('./api/removeGroup.js'))
-
-const http = require("http").Server(app);
-let server = http.listen(3000, () => {
-    let port = server.address().port;
-    console.log("Server started");
-    console.log("Server listening on port: " + port);
 })
