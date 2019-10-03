f6de869d3c6b161145c9dfa554647d99715d04cd
connorf25 <connorforbes25@gmail.com>
2019-10-03 18:38:03 +1000

    Profile page style

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -18,6 +18,8 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.user = JSON.parse(sessionStorage.getItem('user'))
+    if (this.user && (typeof this.user.pic == 'undefined' || this.user.pic))
+      this.user.pic = 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/social/thumbnail/apple-id-account-person-thumbnail-2x.png'
     if (!this.user || this.user.username == "" || !this.user.valid) {
       this.router.navigateByUrl('/login')
     }
