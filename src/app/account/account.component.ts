caf35ccad9de563b2f2e444f9dcf0a8b9ef9451a
connorf25 <connorforbes25@gmail.com>
2019-10-03 18:26:05 +1000

    Account page style

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { User } from '../services/user.class';
 
 @Component({
   selector: 'app-account',
@@ -7,12 +8,14 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
-  user: any;
+  user: User;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.user = JSON.parse(sessionStorage.getItem('user'))
+    if (this.user && (typeof this.user.pic == 'undefined' || this.user.pic))
+      this.user.pic = 'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/social/thumbnail/apple-id-account-person-thumbnail-2x.png'
   }
   
 
